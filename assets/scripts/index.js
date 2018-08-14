d09bf08c2928d1210eb5e738d1736907ec3776a9
subdit <subdit@gmail.com>
2018-08-14 02:41:50 -0400

    update code

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,61 +1,62 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+ // use require with a reference to bundle the file and use it in this file
+ // const example = require('./example')
 
-// use require without a reference to ensure a file is bundled
-// require('./example')
+ // use require without a reference to ensure a file is bundled
+ // require('./example')
 
 $(() => {
 
 })
-// let boardGame
-// const player = 'X'
+ let boardGame
+ const player = 'X'
 
-//  function gameStart () {
-//  player.turn = 'X'
-//    setMessage(player.turn + 'get started.')
-//  }
-//  function setMessage (msg) {
-//    player.getElementByID('message').innerText = msg
-//  }
-//  function nextMove (square) {
-//    if (square.innerText === '') {
-//      square.innerText = player.turn
-//      switchTurn()
-//    } else {
-//      setMessage('Pick new square')
-//    }
-//   function switchTurn () {
-//    if (player.turn === 'X') {
-//        computer.turn = 'O'
-//    } else {
-//      player.turn = 'X'
-//    }
-//    function winningGame() {
-//      [id1, id2, id3],
-//      [id4, id5, id6],
-//      [id7, id8, id9],
-//      [id1, id4, id7],
-//      [id2, id5, id8],
-//      [id3, id6, id9],
-//      [id1, id5, id9],
-//      [id3, id5, id7]
-//    }
+  function gameStart () {
+  player.turn = 'X'
+    setMessage(player.turn + 'get started.')
+  }
+  function setMessage (msg) {
+    player.getElementByID('message').innerText = msg
+  }
+  function nextMove (square) {
+    if (square.innerText === '') {
+      square.innerText = player.turn
+      switchTurn()
+    } else {
+      setMessage('Pick new square')
+    }
+   function switchTurn () {
+    if (player.turn === 'X') {
+        computer.turn = 'O'
+    } else {
+      player.turn = 'X'
+    }
+    function winningGame() {
+      [id1, id2, id3],
+      [id4, id5, id6],
+      [id7, id8, id9],
+      [id1, id4, id7],
+      [id2, id5, id8],
+      [id3, id6, id9],
+      [id1, id5, id9],
+      [id3, id5, id7]
+    }
 
-//      function winner = ('winningGame') {
-//        let (winner === winningGame)
-//        setMessage('You win')
-//      } else{
-//        gamePlayOver!==='winningGame'
-//        setMessage('You lose')
-//      }
-//      function reset(){
-//      reset = ['', '', '']
-//              ['', '', ''],
-//              ['', '', '']
-//      }
-//      if(gamePlayOver.reset)
-//      setMessage(player.message + 'Hit Game Reset button.')
-//   }
-// } })
+      function winner = ('winningGame') {
+        let (winner === winningGame)
+        setMessage('You win')
+      } else{
+        gamePlayOver!==='winningGame'
+        setMessage('You lose')
+      }
+      function reset(){
+      reset = ['', '', '']
+              ['', '', ''],
+              ['', '', '']
+      }
+      if(gamePlayOver.reset)
+      setMessage(player.message + 'Hit Game Reset button.')
+   }
+ }
+})
