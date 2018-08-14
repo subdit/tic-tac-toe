8ddbae26ef7e733c9971b07982864399a5cc281f
subdit <subdit@gmail.com>
2018-08-14 03:12:15 -0400

    thing need works

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,54 +9,52 @@
 $(() => {
 
 })
- let boardGame
- const player = 'X'
+// let boardGame
+//  const player = 'X'
 
-  function gameStart () {
-  player.turn = 'X'
-    setMessage(player.turn + 'get started.')
-  }
-  function setMessage (msg) {
-    player.getElementByID('message').innerText = msg
-  }
-  function nextMove (square) {
-    if (square.innerText === '') {
-      square.innerText = player.turn
-      switchTurn()
-    } else {
-      setMessage('Pick new square')
-    }
-   function switchTurn () {
-    if (player.turn === 'X') {
-        computer.turn = 'O'
-    } else {
-      player.turn = 'X'
-    }
-    function winningGame() {
-      [id1, id2, id3],
-      [id4, id5, id6],
-      [id7, id8, id9],
-      [id1, id4, id7],
-      [id2, id5, id8],
-      [id3, id6, id9],
-      [id1, id5, id9],
-      [id3, id5, id7]
-    }
+//   function gameStart () {
+//   player.turn = 'X'
+//    setMessage(player.turn + 'get started.')
+//   }
+//   function setMessage (msg) {
+//    player.getElementByID('message').innerText = msg
+//   }
+//   function nextMove (square) {
+//    if (square.innerText === '') {
+//      square.innerText = player.turn
+//      switchTurn()
+//    } else {
+//      setMessage('Pick new square')
 
-      function winner = ('winningGame') {
-        let (winner === winningGame)
-        setMessage('You win')
-      } else{
-        gamePlayOver!==='winningGame'
-        setMessage('You lose')
-      }
-      function reset(){
-      reset = ['', '', '']
-              ['', '', ''],
-              ['', '', '']
-      }
-      if(gamePlayOver.reset)
-      setMessage(player.message + 'Hit Game Reset button.')
-   }
- }
-})
+//   }
+//   function switchTurn () {
+//    if (player.turn === 'X') {
+//        computer.turn = 'O'
+//    } else {
+//      player.turn = 'X'
+//      }
+//    winningGame function () {
+//        [id1, id2, id3],
+//        [id4, id5, id6],
+//        [id7, id8, id9],
+//        [id1, id4, id7],
+//        [id2, id5, id8],
+//        [id3, id6, id9],
+//        [id1, id5, id9],
+//        [id3, id5, id7]
+//      }
+
+//      function winner = ('winningGame') {
+//        let (winner === winningGame)
+//        setMessage('You win')
+//      } else{
+//        gamePlayOver!==='winningGame'
+//      setMessage('You lose')
+//     }
+//      function reset() {
+//              reset = ['', '', '']
+//              ['', '', ''],
+//              ['', '', '']
+//      }
+//    if(gamePlayOver.reset)
+//    {setMessage(player.message + 'Hit Game Reset button.')}
